6a9df57c3a0edf78d53e712ee76ffdb4a60ce24c
Blake <blake@towerfarm.com>
2021-09-20 19:45:45 -0700

    Red ball test

--- triangle.js
+++ triangle.js
@@ -1,56 +1,87 @@
+const environ = 'pillars_2k';
+const ibl_url = `${environ}/${environ}_ibl.ktx`;
+const sky_url = `${environ}/${environ}_skybox.ktx`;
+const filamat_url = 'plastic.filamat'
+Filament.init([filamat_url, ibl_url, sky_url], () => {
+  // Create some global aliases to enums for convenience.
+  window.VertexAttribute = Filament.VertexAttribute;
+  window.AttributeType = Filament.VertexBuffer$AttributeType;
+  window.PrimitiveType = Filament.RenderableManager$PrimitiveType;
+  window.IndexType = Filament.IndexBuffer$IndexType;
+  window.Fov = Filament.Camera$Fov;
+  window.LightType = Filament.LightManager$Type;
+  // Obtain the canvas DOM object and pass it to the App.
+  const canvas = document.getElementsByTagName('canvas')[0];
+  window.app = new App(canvas);
+});
 class App {
-  constructor() {
-    this.canvas = document.getElementsByTagName('canvas')[0];
-    const engine = this.engine = Filament.Engine.create(this.canvas);
-    this.scene = engine.createScene();
-    this.triangle = Filament.EntityManager.get()
+  constructor(canvas) {
+    this.canvas = canvas;
+    const engine = this.engine = Filament.Engine.create(canvas);
+    const scene = engine.createScene();
+    const material = engine.createMaterial(filamat_url);
+    const matinstance = material.createInstance();
+    const red = [0.8, 0.0, 0.0];
+    matinstance.setColor3Parameter('baseColor', Filament.RgbType.sRGB, red);
+    matinstance.setFloatParameter('roughness', 0.5);
+    matinstance.setFloatParameter('clearCoat', 1.0);
+    matinstance.setFloatParameter('clearCoatRoughness', 0.3);
+    const renderable = Filament.EntityManager.get()
       .create();
-    this.scene.addEntity(this.triangle);
-    const TRIANGLE_POSITIONS = new Float32Array([
-      1, 0,
-      Math.cos(Math.PI * 2 / 3), Math.sin(Math.PI * 2 / 3),
-      Math.cos(Math.PI * 4 / 3), Math.sin(Math.PI * 4 / 3),
-    ]);
-    const TRIANGLE_COLORS = new Uint32Array([0xffff0000, 0xff00ff00, 0xff0000ff]);
-    const VertexAttribute = Filament.VertexAttribute;
-    const AttributeType = Filament.VertexBuffer$AttributeType;
-    this.vb = Filament.VertexBuffer.Builder()
-      .vertexCount(3)
+    scene.addEntity(renderable);
+    const icosphere = new Filament.IcoSphere(5);
+    const vb = Filament.VertexBuffer.Builder()
+      .vertexCount(icosphere.vertices.length / 3)
       .bufferCount(2)
-      .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT2, 0, 8)
-      .attribute(VertexAttribute.COLOR, 1, AttributeType.UBYTE4, 0, 4)
-      .normalized(VertexAttribute.COLOR)
+      .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT3, 0, 0)
+      .attribute(VertexAttribute.TANGENTS, 1, AttributeType.SHORT4, 0, 0)
+      .normalized(VertexAttribute.TANGENTS)
       .build(engine);
-    this.vb.setBufferAt(engine, 0, TRIANGLE_POSITIONS);
-    this.vb.setBufferAt(engine, 1, TRIANGLE_COLORS);
-    this.ib = Filament.IndexBuffer.Builder()
-      .indexCount(3)
-      .bufferType(Filament.IndexBuffer$IndexType.USHORT)
+    const ib = Filament.IndexBuffer.Builder()
+      .indexCount(icosphere.triangles.length)
+      .bufferType(IndexType.USHORT)
       .build(engine);
-    this.ib.setBuffer(engine, new Uint16Array([0, 1, 2]));
-    const mat = engine.createMaterial('triangle.filamat');
-    const matinst = mat.getDefaultInstance();
+    vb.setBufferAt(engine, 0, icosphere.vertices);
+    vb.setBufferAt(engine, 1, icosphere.tangents);
+    ib.setBuffer(engine, icosphere.triangles);
     Filament.RenderableManager.Builder(1)
       .boundingBox({
         center: [-1, -1, -1],
         halfExtent: [1, 1, 1]
       })
-      .material(0, matinst)
-      .geometry(0, Filament.RenderableManager$PrimitiveType.TRIANGLES, this.vb, this.ib)
-      .build(engine, this.triangle);
+      .material(0, matinstance)
+      .geometry(0, PrimitiveType.TRIANGLES, vb, ib)
+      .build(engine, renderable);
+    const sunlight = Filament.EntityManager.get()
+      .create();
+    scene.addEntity(sunlight);
+    Filament.LightManager.Builder(LightType.SUN)
+      .color([0.98, 0.92, 0.89])
+      .intensity(110000.0)
+      .direction([0.6, -1.0, -0.8])
+      .sunAngularRadius(1.9)
+      .sunHaloSize(10.0)
+      .sunHaloFalloff(80.0)
+      .build(engine, sunlight);
+    const backlight = Filament.EntityManager.get()
+      .create();
+    scene.addEntity(backlight);
+    Filament.LightManager.Builder(LightType.DIRECTIONAL)
+      .direction([-1, 0, 1])
+      .intensity(50000.0)
+      .build(engine, backlight);
+    const indirectLight = engine.createIblFromKtx(ibl_url);
+    indirectLight.setIntensity(50000);
+    scene.setIndirectLight(indirectLight);
+    const skybox = engine.createSkyFromKtx(sky_url);
+    scene.setSkybox(skybox);
     this.swapChain = engine.createSwapChain();
     this.renderer = engine.createRenderer();
     this.camera = engine.createCamera(Filament.EntityManager.get()
       .create());
     this.view = engine.createView();
     this.view.setCamera(this.camera);
-    this.view.setScene(this.scene);
-    // Set up a blue-green background:
-    this.renderer.setClearOptions({
-      clearColor: [0.0, 0.1, 0.2, 1.0],
-      clear: true
-    });
-    // Adjust the initial viewport:
+    this.view.setScene(scene);
     this.resize();
     this.render = this.render.bind(this);
     this.resize = this.resize.bind(this);
@@ -58,14 +89,12 @@ class App {
     window.requestAnimationFrame(this.render);
   }
   render() {
-    // Rotate the triangle.
-    const radians = Date.now() / 1000;
-    const transform = mat4.fromRotation(mat4.create(), radians, [0, 0, 1]);
-    const tcm = this.engine.getTransformManager();
-    const inst = tcm.getInstance(this.triangle);
-    tcm.setTransform(inst, transform);
-    inst.delete();
-    // Render the frame.
+    const eye = [0, 0, 4],
+      center = [0, 0, 0],
+      up = [0, 1, 0];
+    const radians = Date.now() / 10000;
+    vec3.rotateY(eye, eye, center, radians);
+    this.camera.lookAt(eye, center, up);
     this.renderer.render(this.swapChain, this.view);
     window.requestAnimationFrame(this.render);
   }
@@ -74,11 +103,6 @@ class App {
     const width = this.canvas.width = window.innerWidth * dpr;
     const height = this.canvas.height = window.innerHeight * dpr;
     this.view.setViewport([0, 0, width, height]);
-    const aspect = width / height;
-    const Projection = Filament.Camera$Projection;
-    this.camera.setProjection(Projection.ORTHO, -aspect, aspect, -1, 1, 0, 1);
+    this.camera.setProjectionFov(45, width / height, 1.0, 10.0, Fov.VERTICAL);
   }
 }
-Filament.init(['triangle.filamat'], () => {
-  window.app = new App()
-});
