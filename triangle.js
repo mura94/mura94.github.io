492e1252c98cdd6233e4aa057bf43a4ccfec045b
Blake <blake@towerfarm.com>
2021-09-20 20:08:26 -0700

    Test monkey

--- triangle.js
+++ triangle.js
@@ -1,100 +1,80 @@
-const environ = 'pillars_2k';
+const albedo_suffix = Filament.getSupportedFormatSuffix('astc s3tc_srgb');
+const texture_suffix = Filament.getSupportedFormatSuffix('etc');
+const environ = 'venetian_crossroads_2k'
 const ibl_url = `${environ}/${environ}_ibl.ktx`;
-const sky_url = `${environ}/${environ}_skybox.ktx`;
-const filamat_url = 'plastic.filamat'
-Filament.init([filamat_url, ibl_url, sky_url], () => {
-  // Create some global aliases to enums for convenience.
-  window.VertexAttribute = Filament.VertexAttribute;
-  window.AttributeType = Filament.VertexBuffer$AttributeType;
-  window.PrimitiveType = Filament.RenderableManager$PrimitiveType;
-  window.IndexType = Filament.IndexBuffer$IndexType;
-  window.Fov = Filament.Camera$Fov;
-  window.LightType = Filament.LightManager$Type;
-  // Obtain the canvas DOM object and pass it to the App.
-  const canvas = document.getElementsByTagName('canvas')[0];
-  window.app = new App(canvas);
+const sky_small_url = `${environ}/${environ}_skybox_tiny.ktx`;
+const sky_large_url = `${environ}/${environ}_skybox.ktx`;
+const albedo_url = `albedo${albedo_suffix}.ktx`;
+const ao_url = `ao${texture_suffix}.ktx`;
+const metallic_url = `metallic${texture_suffix}.ktx`;
+const normal_url = `normal${texture_suffix}.ktx`;
+const roughness_url = `roughness${texture_suffix}.ktx`;
+const filamat_url = 'textured.filamat';
+const filamesh_url = 'suzanne.filamesh';
+Filament.init([filamat_url, filamesh_url, sky_small_url, ibl_url], () => {
+  window.app = new App(document.getElementsByTagName('canvas')[0]);
 });
 class App {
   constructor(canvas) {
     this.canvas = canvas;
-    const engine = this.engine = Filament.Engine.create(canvas);
-    const scene = engine.createScene();
-    const material = engine.createMaterial(filamat_url);
-    const matinstance = material.createInstance();
-    const red = [0.8, 0.0, 0.0];
-    matinstance.setColor3Parameter('baseColor', Filament.RgbType.sRGB, red);
-    matinstance.setFloatParameter('roughness', 0.5);
-    matinstance.setFloatParameter('clearCoat', 1.0);
-    matinstance.setFloatParameter('clearCoatRoughness', 0.3);
-    const renderable = Filament.EntityManager.get()
-      .create();
-    scene.addEntity(renderable);
-    const icosphere = new Filament.IcoSphere(5);
-    const vb = Filament.VertexBuffer.Builder()
-      .vertexCount(icosphere.vertices.length / 3)
-      .bufferCount(2)
-      .attribute(VertexAttribute.POSITION, 0, AttributeType.FLOAT3, 0, 0)
-      .attribute(VertexAttribute.TANGENTS, 1, AttributeType.SHORT4, 0, 0)
-      .normalized(VertexAttribute.TANGENTS)
-      .build(engine);
-    const ib = Filament.IndexBuffer.Builder()
-      .indexCount(icosphere.triangles.length)
-      .bufferType(IndexType.USHORT)
-      .build(engine);
-    vb.setBufferAt(engine, 0, icosphere.vertices);
-    vb.setBufferAt(engine, 1, icosphere.tangents);
-    ib.setBuffer(engine, icosphere.triangles);
-    Filament.RenderableManager.Builder(1)
-      .boundingBox({
-        center: [-1, -1, -1],
-        halfExtent: [1, 1, 1]
-      })
-      .material(0, matinstance)
-      .geometry(0, PrimitiveType.TRIANGLES, vb, ib)
-      .build(engine, renderable);
-    const sunlight = Filament.EntityManager.get()
-      .create();
-    scene.addEntity(sunlight);
-    Filament.LightManager.Builder(LightType.SUN)
-      .color([0.98, 0.92, 0.89])
-      .intensity(110000.0)
-      .direction([0.6, -1.0, -0.8])
-      .sunAngularRadius(1.9)
-      .sunHaloSize(10.0)
-      .sunHaloFalloff(80.0)
-      .build(engine, sunlight);
-    const backlight = Filament.EntityManager.get()
-      .create();
-    scene.addEntity(backlight);
-    Filament.LightManager.Builder(LightType.DIRECTIONAL)
-      .direction([-1, 0, 1])
-      .intensity(50000.0)
-      .build(engine, backlight);
-    const indirectLight = engine.createIblFromKtx(ibl_url);
-    indirectLight.setIntensity(50000);
-    scene.setIndirectLight(indirectLight);
-    const skybox = engine.createSkyFromKtx(sky_url);
-    scene.setSkybox(skybox);
-    this.swapChain = engine.createSwapChain();
-    this.renderer = engine.createRenderer();
-    this.camera = engine.createCamera(Filament.EntityManager.get()
+    this.engine = Filament.Engine.create(canvas);
+    this.scene = this.engine.createScene();
+    const material = this.engine.createMaterial(filamat_url);
+    this.matinstance = material.createInstance();
+    const filamesh = this.engine.loadFilamesh(filamesh_url, this.matinstance);
+    this.suzanne = filamesh.renderable;
+    this.skybox = this.engine.createSkyFromKtx(sky_small_url);
+    this.scene.setSkybox(this.skybox);
+    this.indirectLight = this.engine.createIblFromKtx(ibl_url);
+    this.indirectLight.setIntensity(100000);
+    this.scene.setIndirectLight(this.indirectLight);
+    this.trackball = new Trackball(canvas, {
+      startSpin: 0.035
+    });
+    Filament.fetch([sky_large_url, albedo_url, roughness_url, metallic_url, normal_url, ao_url],
+    () => {
+      const albedo = this.engine.createTextureFromKtx(albedo_url, {
+        srgb: true
+      });
+      const roughness = this.engine.createTextureFromKtx(roughness_url);
+      const metallic = this.engine.createTextureFromKtx(metallic_url);
+      const normal = this.engine.createTextureFromKtx(normal_url);
+      const ao = this.engine.createTextureFromKtx(ao_url);
+      const sampler = new Filament.TextureSampler(Filament.MinFilter.LINEAR_MIPMAP_LINEAR,
+        Filament.MagFilter.LINEAR, Filament.WrapMode.CLAMP_TO_EDGE);
+      this.matinstance.setTextureParameter('albedo', albedo, sampler);
+      this.matinstance.setTextureParameter('roughness', roughness, sampler);
+      this.matinstance.setTextureParameter('metallic', metallic, sampler);
+      this.matinstance.setTextureParameter('normal', normal, sampler);
+      this.matinstance.setTextureParameter('ao', ao, sampler);
+      // Replace low-res skybox with high-res skybox.
+      this.engine.destroySkybox(this.skybox);
+      this.skybox = this.engine.createSkyFromKtx(sky_large_url);
+      this.scene.setSkybox(this.skybox);
+      this.scene.addEntity(this.suzanne);
+    });
+    this.swapChain = this.engine.createSwapChain();
+    this.renderer = this.engine.createRenderer();
+    this.camera = this.engine.createCamera(Filament.EntityManager.get()
       .create());
-    this.view = engine.createView();
+    this.view = this.engine.createView();
     this.view.setCamera(this.camera);
-    this.view.setScene(scene);
-    this.resize();
+    this.view.setScene(this.scene);
     this.render = this.render.bind(this);
     this.resize = this.resize.bind(this);
     window.addEventListener('resize', this.resize);
-    window.requestAnimationFrame(this.render);
-  }
-  render() {
     const eye = [0, 0, 4],
       center = [0, 0, 0],
       up = [0, 1, 0];
-    const radians = Date.now() / 10000;
-    vec3.rotateY(eye, eye, center, radians);
     this.camera.lookAt(eye, center, up);
+    this.resize();
+    window.requestAnimationFrame(this.render);
+  }
+  render() {
+    const tcm = this.engine.getTransformManager();
+    const inst = tcm.getInstance(this.suzanne);
+    tcm.setTransform(inst, this.trackball.getMatrix());
+    inst.delete();
     this.renderer.render(this.swapChain, this.view);
     window.requestAnimationFrame(this.render);
   }
@@ -103,6 +83,9 @@ class App {
     const width = this.canvas.width = window.innerWidth * dpr;
     const height = this.canvas.height = window.innerHeight * dpr;
     this.view.setViewport([0, 0, width, height]);
-    this.camera.setProjectionFov(45, width / height, 1.0, 10.0, Fov.VERTICAL);
+    const aspect = width / height;
+    const Fov = Filament.Camera$Fov,
+      fov = aspect < 1 ? Fov.HORIZONTAL : Fov.VERTICAL;
+    this.camera.setProjectionFov(45, aspect, 1.0, 10.0, fov);
   }
 }
